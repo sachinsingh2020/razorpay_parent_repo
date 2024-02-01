f9bb74e60b89c5c0247df86a3e850876068f37cc
sachin singh <sachinthegreat1001@gmail.com>
2024-02-01 17:40:54 +0530

    parent razorpay

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import PaymentSuccess from './components/PaymentSuccess'
+
+const App = () => {
+
+
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/paymentsuccess" element={<PaymentSuccess />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ frontend/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ChakraProvider } from "@chakra-ui/react";
+import { ColorModeScript, theme } from '@chakra-ui/react';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <ColorModeScript theme={theme} />
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
+
